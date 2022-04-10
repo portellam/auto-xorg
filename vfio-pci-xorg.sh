af2f49f4689dd3ec32e5950e4fda54601aa26177
Alex Portell <amportell.13@gmail.com>
2022-04-10 00:01:13 +0000

    vfio-pci-set-xorg-device.sh

--- vfio-pci-xorg.sh
+++ vfio-pci-xorg.sh
@@ -18,89 +18,93 @@
 # -delete logfile
 ##
 
-# datetime #
-datetime="#`date +%Y-%m-%d_%H:%M:%S`"
-#
-
-# logfile #
-lspci -nnk > /var/log/lspci.log
-file='/var/log/lspci.log'
-#
-
-#
-while IFS= read -r line; do
-    # variables
-    read line
-    echo $line                              # DEBUG
-    count=0
-    bool_one=false
-    bool_three=false
-    str_one=${line:10:8}
-    str_three=${line:21:0}
-    
-    # begin line one.
-    if $str_one -eq "VGA"; then
-        ((bool_one=true))
-    fi 
-    if $bool_one; then
-        echo $line                          # DEBUG
-        PCI_ID=${line:6:1}                  # EXAMPLE: '01:00.0 VGA'    => '01:00.0'
-        echo $PCI_ID                        # DEBUG
-        sub1=${PCI_ID:6:5}+:${PCI_ID:6:6}   # EXAMPLE: '01:00.0'        => '01:00:0'
-        echo $sub1                          # DEBUG
-        sub2=${sub1:2:0}+${sub2:6:4}        # EXAMPLE: '01:00:0'        => '01:0:0'
-        echo $sub2                          # DEBUG
-        newPCI_ID=${sub2:5:1}               # EXAMPLE: '01:0:0'         => '1:0:0'
-        echo $newPCI_ID                     # DEBUG
-        # move on to line three.
-        ((bool_one=false))
-    fi
-    # end of line one.
-    
-    # begin line three
-    if $str_three -eq 'Kernel driver in use: '; then
-        ((bool_three=true))
-    fi
-    if $bool_three; then
-        echo $line                          # DEBUG
-        kernel_driver=${line:${#line}:21}
-        echo $kernel_driver                 # DEBUG
-        # if current kernel driver is vfio-pci, then go back to line one.
-        if $kernel_driver -eq 'vfio-pci'; then
-            ((bool_three=false))        
-        # if current kernel driver is NOT vfio-pci...
-        else
-            echo "'/etc/X11/xorg.conf.d/': set all VGA kernel driver files to NOT readable (0200)."     
-            # set all XORG conf's of VGA kernel drivers to NOT readable.
-            chmod 200 /etc/X11/xorg.conf.d/10-*.conf    
-            # check if current VGA kernel driver's XORG conf exists.
-            if -f "/etc/X11/xorg.conf.d/10-$kernel_driver"; then
-                echo "'/etc/X11/xorg.conf.d/10-$kernel_driver': file exists."
-            # if NOT, create one.
-            else
-                echo "'/etc/X11/xorg.conf.d/10-$kernel_driver': file does NOT exist. Creating file..."
-                cat > /etc/X11/xorg.conf.d/10-$kernel_driver.conf < EOF
-datetime
+## FUNCTIONS ##
+setup_xorg() {
+    echo "'/etc/X11/xorg.conf.d/': set all VGA kernel driver files to NOT readable (0200)."
+    chmod 200 /etc/X11/xorg.conf.d/10-*.conf    # set all XORG conf's of VGA kernel drivers to NOT readable.
+    if -f "/etc/X11/xorg.conf.d/10-$kernel_driver"; then    # check if current VGA kernel driver's XORG conf exists.
+        echo "'/etc/X11/xorg.conf.d/10-$kernel_driver': file exists."
+        else    # if NOT, create one.
+        echo "'/etc/X11/xorg.conf.d/10-$kernel_driver': file does NOT exist. Creating file..."
+        cat > /etc/X11/xorg.conf.d/10-$kernel_driver.conf < EOF
 Section "Device"
 Identifier     "Device0"
 Driver         "$kernel_driver"
 BusID          "PCI:$PCI_ID"
 EndSection
 EOF
-            fi
-            # then set to executable.
-            echo "'/etc/X11/xorg.conf.d/10-$kernel_driver': set file to readable (0644)."
-            chmod 644 /etc/X11/xorg.conf.d/10-$kernel_driver.conf
-            # then restart display manager (?).
-            #systemctl restart lightdm sddm
-            break
-        fi
     fi
-    # end of line three.
-done
-#
+    echo "'/etc/X11/xorg.conf.d/10-$kernel_driver': set file to readable (0644)."   # then set to executable.
+    chmod 644 /etc/X11/xorg.conf.d/10-$kernel_driver.conf
+}
 
-# delete logfile #
+new_logfile () {
+    lspci -nnk > /var/log/lscpi.log
+    return /var/log/lspci.log
+}
+
+find_line () {
+    while IFS= read -r line; do
+        echo $line              # DEBUG
+        if [[ ${line:8:3} -eq "VGA" ]]; then
+            PCI_ID=${line:1:5}                  # EXAMPLE: '01:00.0 VGA'    => '01:00.0'
+            echo "$PCI_ID"      # DEBUG
+            sub1=${PCI_ID:5:2}+:${PCI_ID:6:1}   # EXAMPLE: '01:00.0'        => '01:00:0'
+            echo "$sub1"        # DEBUG
+            sub2=${sub1:0:3}+${sub2:4:3}        # EXAMPLE: '01:00:0'        => '01:0:0'
+            echo "$sub2"        # DEBUG
+            newPCI_ID=${sub2:1:6}               # EXAMPLE: '01:0:0'         => '1:0:0'
+            echo "$newPCI_ID"   # DEBUG
+        fi
+        if [[ ${$:0:22}='Kernel driver in use: ' ]]; then
+            n=${#line}-21
+            kernel_driver=${line:21:n}
+            echo $kernel_driver # DEBUG
+            if [[ $kernel_driver -ne "vfio-pci" ]]; then
+                setup_xorg ()
+            fi
+        fi
+    done
+}
+
+fine_line_2 () {
+    echo $line              # DEBUG
+        if [[ ${line:8:3} -eq "VGA" ]] then
+            PCI_ID=${line:1:5}                  # EXAMPLE: '01:00.0 VGA'    => '01:00.0'
+            echo "$PCI_ID"      # DEBUG
+            sub1=${PCI_ID:5:2}+:${PCI_ID:6:1}   # EXAMPLE: '01:00.0'        => '01:00:0'
+            echo "$sub1"        # DEBUG
+            sub2=${sub1:0:3}+${sub2:4:3}        # EXAMPLE: '01:00:0'        => '01:0:0'
+            echo "$sub2"        # DEBUG
+            newPCI_ID=${sub2:1:6}               # EXAMPLE: '01:0:0'         => '1:0:0'
+            echo "$newPCI_ID"   # DEBUG
+        fi
+        if [[ ${$:0:22}='Kernel driver in use: ' ]] then
+            n=${#line}-21
+            kernel_driver=${line:21:n}
+            echo $kernel_driver # DEBUG
+            if [[ $kernel_driver -ne "vfio-pci" ]] then
+                setup_xorg ()
+            fi
+        fi
+}
+
+restart_dm () {
+    cat /etc/X11/default-display-manager > $line
+    echo $line  # DEBUG
+    dm=${line:8:(${#line}-9)}
+    echo $dm    # DEBUG
+    systemctl restart $dm
+}
+##
+
+## MAIN ##
+new_logfile ()
+#find_line ()
+while IFS= read -r line; do
+    find_line_2 ()
+done
+restart_dm ()
 rm $file
-#
 exit 0
+##
