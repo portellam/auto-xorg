fb43731b70e01e2934f3e4dc91bc45eac37f2d7a
portellam <amportell.13@gmail.com>
2023-08-14 06:34:33 -0400

    Updated shebang. Refactored code. Carried over logic from work on acpi-sleep: loggers, error handling, validation.

--- installer.bash
+++ installer.bash
@@ -1,4 +1,4 @@
-#!/bin/bash sh
+#!/bin/bash/env bash
 
 #
 # Filename:       installer.bash
@@ -8,6 +8,12 @@
 #
 
 # <params>
+  SAVEIFS="${IFS}"
+  IFS=$'\n'
+
+  SCRIPT_NAME="$( basename "${0}" )"
+  PREFIX_PROMPT="${SCRIPT_NAME}: "
+
   OPTION_STRING=""
   SORT_OPTION=""
   VENDOR_OPTION=""
@@ -22,10 +28,9 @@
   readonly RESET_COLOR='\033[0m'
 
   # <summary>Append output</summary>
-  readonly PREFIX_NOTE="${SET_COLOR_YELLOW}Note:${RESET_COLOR}"
-  readonly PREFIX_ERROR="${SET_COLOR_YELLOW}An error occurred:${RESET_COLOR}"
-  readonly PREFIX_FAIL="${SET_COLOR_RED}Failure:${RESET_COLOR}"
-  readonly PREFIX_PASS="${SET_COLOR_GREEN}Success:${RESET_COLOR}"
+  readonly PREFIX_NOTE="${SET_COLOR_YELLOW}Note:${RESET_COLOR} "
+  readonly PREFIX_ERROR="${SET_COLOR_RED}An error occurred:${RESET_COLOR} "
+  readonly PREFIX_PASS="${SET_COLOR_GREEN}Success:${RESET_COLOR} "
 
   readonly PATH_1="/usr/local/bin/"
   readonly PATH_2="/etc/systemd/system/"
@@ -35,238 +40,256 @@
 # </params>
 
 # <functions>
-  function Main
+  function main
   {
-    if ! IsUserSudo \
-      || ! SetOptions "$@" \
-      || ! SaveOptions \
-      || ! IsSourceFileMissing "${FILE_1}" \
-      || ! IsSourceFileMissing "${FILE_2}" \
-      || ! WriteFile2 \
-      || ! SetPermissionsForSourceFiles \
-      || ! IsDestinationPathMissing "${PATH_1}" \
-      || ! IsDestinationPathMissing "${PATH_2}" \
-      || ! CopyFiles \
-      || ! SetPermissionsForDestinationFiles \
-      || ! UpdateServices; then
-      echo -e "${PREFIX_FAIL} Could not install auto-Xorg."
+    if ! is_user_superuser \
+      || ! set_options "$@" \
+      || ! save_options \
+      || ! is_source_file_missing "${FILE_1}" \
+      || ! is_source_file_missing "${FILE_2}" \
+      || ! write_file2 \
+      || ! set_permissions_for_source_files \
+      || ! is_destination_path_found "${PATH_1}" \
+      || ! is_destination_path_found "${PATH_2}" \
+      || ! copy_files \
+      || ! set_permissions_for_destination_files \
+      || ! update_services; then
+      print_to_error_log "Could not install auto-Xorg."
       exit 1
     fi
 
-    echo -e "${PREFIX_PASS} Installed auto-xorg."
-    echo -e "${PREFIX_NOTE} It is NOT necessary to directly execute script '${FILE_1}'"
+    print_to_output_log "${PREFIX_PASS}Installed auto-xorg."
+    echo -e "${PREFIX_NOTE} It is NOT necessary to directly execute script '${FILE_1}'."
     echo -e "The service '${FILE_2}' will execute the script automatically at boot, to grab the first non-VFIO VGA device."
     echo -e "If no available VGA device is found, an Xorg template will be created."
     echo -e "Therefore, it will be assumed the system is running 'headless'."
     exit 0
   }
 
-  function GetOption
-  {
-    while [[ "${1}" =~ ^- \
-      && ! "${1}" == "--" ]]; do
-      case "${1}" in
-        "-f" | "--first" )
-          SetOptionForSort "${1}" || return 1 ;;
+  # <summary>Clean-up</summary>
+    function reset_ifs
+    {
+      IFS="${SAVEIFS}"
+    }
 
-        "-l" | "--last" )
-          SetOptionForSort "${1}" || return 1 ;;
+  # <summary>Data-type validation</summary>
+    function is_string
+    {
+      if [[ "${1}" == "" ]]; then
+        return 1
+      fi
+    }
 
-        "-r" | "--restart-display" )
-          OPTION_STRING="${1} " ;;
+  # <summary>Handlers</summary>
+    function catch_error {
+      exit 255
+    }
 
-        "-a" | "--amd" )
-          SetOptionForVendor "${1}" || return 1 ;;
+    function catch_exit {
+      reset_ifs
+    }
 
-        "-i" | "--intel" )
-          SetOptionForVendor "${1}" || return 1 ;;
+    function is_user_superuser
+    {
+      if [[ $( whoami ) != "root" ]]; then
+        print_to_error_log "User is not sudo or root."
+        return 1
+      fi
+    }
 
-        "-n" | "--nvidia" )
-          SetOptionForVendor "${1}" || return 1 ;;
+  # <summary>Loggers</summary>
+    function print_to_error_log
+    {
+      echo -e "${PREFIX_PROMPT}${PREFIX_ERROR}${1}" >&2
+    }
 
-        "-o" | "--other" )
-          SetOptionForVendor "${1}" || return 1 ;;
+    function print_to_output_log
+    {
+      echo -e "${PREFIX_PROMPT}${1}" >&1
+    }
 
-        "" )
-          ;;
+  # <summary>Options logic</summary>
+    function get_option
+    {
+      while [[ "${1}" =~ ^- \
+        && ! "${1}" == "--" ]]; do
+        case "${1}" in
+          "-f" | "--first" )
+            set_option_for_sort "${1}" || return 1 ;;
 
-        "-h" | "--help" )
-          PrintUsage
-          exit 1 ;;
+          "-l" | "--last" )
+            set_option_for_sort "${1}" || return 1 ;;
 
-        * )
-          PrintUsage
-          return 1 ;;
-      esac
+          "-r" | "--restart-display" )
+            OPTION_STRING="${1} " ;;
 
-      shift
-    done
+          "-a" | "--amd" )
+            set_option_for_vendor "${1}" || return 1 ;;
 
-    if [[ "${1}" == '--' ]]; then
-      shift
-    fi
+          "-i" | "--intel" )
+            set_option_for_vendor "${1}" || return 1 ;;
 
-    return 0
-  }
+          "-n" | "--nvidia" )
+            set_option_for_vendor "${1}" || return 1 ;;
 
-  function PrintUsage
-  {
-    IFS=$'\n'
+          "-o" | "--other" )
+            set_option_for_vendor "${1}" || return 1 ;;
 
-    local -ar output=(
-      "Usage: sudo bash installer.bash [OPTION]..."
-      "  Set options for auto-Xorg in service file, then install."
-      "\n    -h, --help\t\tPrint this help and exit."
-      "\n  Update Xorg:"
-      "    -r, --restart-display\tRestart the display manager immediately."
-      "\n  Set device order:"
-      "    -f, --first\t\tFind the first valid VGA device."
-      "    -l, --last\t\tFind the last valid VGA device."
-      "\n  Prefer a vendor:"
-      "    -a, --amd\t\tAMD or ATI"
-      "    -i, --intel\t\tIntel"
-      "    -n, --nvidia\t\tNVIDIA"
-      "    -o, --other\t\tAny other brand (past or future)."
-      "\n  Example:"
-      "    sudo bash installer.bash -f -a\tSet options to find first valid AMD/ATI VGA device, then install."
-      "    sudo bash installer.bash -l -n -r\tSet options to find last valid NVIDIA VGA device, and restart the display manager, then install."
-    )
+          "" )
+            ;;
 
-    echo -e "${output[*]}"
-    unset IFS
-    return 0
-  }
+          "-h" | "--help" )
+            print_usage
+            exit 1 ;;
 
-  function SetOptionForSort
-  {
-    if [[ "${SORT_OPTION}" != "" ]]; then
-      echo -e "${PREFIX_ERROR} Could not add sort option. Sort option is already set."
-      return 1
-    fi
+          * )
+            print_usage
+            return 1 ;;
+        esac
 
-    readonly SORT_OPTION="${1}"
-    return 0
-  }
+        shift
+      done
 
-  function SetOptionForVendor
-  {
-    if [[ "${VENDOR_OPTION}" != "" ]]; then
-      echo -e "${PREFIX_ERROR} Could not add vendor option. Vendor option is already set."
-      return 1
-    fi
+      if [[ "${1}" == '--' ]]; then
+        shift
+      fi
+    }
 
-    readonly VENDOR_OPTION="${1}"
-    return 0
-  }
+    function print_usage
+    {
+      local -ar output=(
+        "Usage: bash ${SCRIPT_NAME} [OPTION]..."
+        "  Set options for auto-xorg in service file, then install."
+        "\n    -h, --help\t\tPrint this help and exit."
+        "\n  Update Xorg:"
+        "    -r, --restart-display\tRestart the display manager immediately."
+        "\n  Set device order:"
+        "    -f, --first\t\tFind the first valid VGA device."
+        "    -l, --last\t\tFind the last valid VGA device."
+        "\n  Prefer a vendor:"
+        "    -a, --amd\t\tAMD or ATI"
+        "    -i, --intel\t\tIntel"
+        "    -n, --nvidia\t\tNVIDIA"
+        "    -o, --other\t\tAny other brand (past or future)."
+        "\n  Example:"
+        "    sudo bash installer.bash -f -a\tSet options to find first valid AMD/ATI VGA device, then install."
+        "    sudo bash installer.bash -l -n -r\tSet options to find last valid NVIDIA VGA device, and restart the display manager, then install."
+      )
 
-  function SetOptions
-  {
-    for option in "$@"; do
-      if [[ "${option}" == "" ]]; then
-        return 0
+      echo -e "${output[*]}"
+    }
+
+    function set_option_for_sort
+    {
+      if [[ "${SORT_OPTION}" != "" ]]; then
+        print_to_error_log "Could not add sort option. Sort option is already set."
+        return 1
       fi
 
-      GetOption "${option}" || return 1
-    done
+      readonly SORT_OPTION="${1}"
+    }
 
-    return 0
-  }
+    function set_option_for_vendor
+    {
+      if [[ "${VENDOR_OPTION}" != "" ]]; then
+        print_to_error_log "Could not add vendor option. Vendor option is already set."
+        return 1
+      fi
 
-  function SaveOptions
-  {
-    if [[ "${SORT_OPTION}" != "" ]]; then
-      OPTION_STRING+="${SORT_OPTION} "
-    fi
+      readonly VENDOR_OPTION="${1}"
+    }
 
-    if [[ "${VENDOR_OPTION}" != "" ]]; then
-      OPTION_STRING+="${VENDOR_OPTION} "
-    fi
+    function set_options
+    {
+      for option in "$@"; do
+        if ! is_string "${option}" &> /dev/null; then
+          return 0
+        fi
 
-    if [[ "${OPTION_STRING}" != "" ]]; then
-      OPTION_STRING="${OPTION_STRING::-1}"
-    fi
+        get_option "${option}" || return 1
+      done
+    }
 
-    readonly OPTION_STRING
-    return 0
-  }
+    function save_options
+    {
+      if is_string "${SORT_OPTION}" &> /dev/null; then
+        OPTION_STRING+="${SORT_OPTION} "
+      fi
 
-  function IsUserSudo
-  {
-    if [[ $( whoami ) != "root" ]]; then
-      echo -e "${PREFIX_ERROR} User is not sudo/root."
-      return 1
-    fi
+      if is_string "${VENDOR_OPTION}" &> /dev/null; then
+        OPTION_STRING+="${VENDOR_OPTION} "
+      fi
 
-    return 0
-  }
+      if is_string "${OPTION_STRING}" &> /dev/null; then
+        OPTION_STRING="${OPTION_STRING::-1}"
+      fi
 
-  function CopyFiles
+      readonly OPTION_STRING
+    }
+
+  function copy_files
   {
     if ! cp --force "${FILE_1}" "${PATH_1}${FILE_1}" &> /dev/null \
       || ! cp --force "${FILE_2}" "${PATH_2}${FILE_2}" &> /dev/null; then
-      echo -e "${PREFIX_ERROR} Failed to copy file(s)."
+      print_to_error_log "Failed to copy file(s)."
       return 1
     fi
 
-    return 0
+    print_to_output_log "Copied file(s)."
   }
 
-  function IsDestinationPathMissing
+  function is_destination_path_found
   {
     if [[ ! -d "${1}" ]]; then
-      echo -e "${PREFIX_ERROR} Could not find directory '${1}'."
+      print_to_error_log "Could not find directory '${1}'."
       return 1
     fi
-
-    return 0
   }
 
-  function IsSourceFileMissing
+  function is_source_file_missing
   {
-    if [[ -z "${1}" ]]; then
-      echo -e "${PREFIX_ERROR} Missing project file '${1}'."
+    if [[ ! -e "${1}" ]]; then
+      print_to_error_log "Missing source file '${1}'."
       return 1
     fi
-
-    return 0
   }
 
-  function SetPermissionsForSourceFiles
+  function set_permissions_for_source_files
   {
     if ! chown --silent ${SUDO_USER}:${SUDO_USER} "${FILE_2}"; then
-      echo -e "${PREFIX_ERROR} Failed to set file permissions."
+      print_to_error_log "Failed to set source file permissions."
       return 1
     fi
 
-    return 0
+    print_to_output_log "Set source file permissions."
   }
 
-  function SetPermissionsForDestinationFiles
+  function set_permissions_for_destination_files
   {
     if ! chown --quiet root:root "${PATH_1}${FILE_1}" \
       || ! chmod --quiet +x "${PATH_1}${FILE_1}" \
       || ! chown --quiet root:root "${PATH_2}${FILE_2}" \
       || ! chmod --quiet +x "${PATH_2}${FILE_2}"; then
-      echo -e "${PREFIX_ERROR} Failed to set file permissions."
+      print_to_error_log "Failed to set destination file permissions."
       return 1
     fi
 
-    return 0
+    print_to_output_log "Set destination file permissions."
   }
 
-  function UpdateServices
+  function update_services
   {
     if ! sudo systemctl daemon-reload &> /dev/null \
       || ! sudo systemctl enable "${FILE_2}" &> /dev/null \
       || ! sudo systemctl restart "${FILE_2}" &> /dev/null; then
-      echo -e "${PREFIX_ERROR} Failed to update systemd with new daemon/service."
+      print_to_error_log "Failed to update services."
       return 1
     fi
 
-    return 0
+    print_to_output_log "Updated services."
   }
 
-  function WriteFile2
+  function write_file2
   {
     line_to_use="${LINE_TO_REPLACE}"
 
@@ -275,6 +298,7 @@
     fi
 
     readonly line_to_use
+
     local -ar file_2_contents=(
       "[Unit]"
       "Description=auto-Xorg"
@@ -288,18 +312,12 @@
       "WantedBy=multi-user.target"
     )
 
-    IFS=$'\n'
-
     if ! echo -e "${file_2_contents[*]}" > "${FILE_2}"; then
-      unset IFS
       return 1
     fi
-
-    unset IFS
-    return 0
   }
 # </functions>
 
 # <code>
-  Main "$@"
+  main "$@"
 # </code>
